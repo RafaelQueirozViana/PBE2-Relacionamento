1349075f6c15fb67a5a7d1c95dfba9dbc3a83682
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-07-29 14:50:37 -0300

    criando atributos da classe tipoQuarto

--- src/main/java/com/recanto/model/tipoQuarto.java
+++ src/main/java/com/recanto/model/tipoQuarto.java
@@ -1,4 +1,22 @@
 package com.recanto.model;
 
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "quarto")
 public class tipoQuarto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer numero;
+
+    @Column(nullable = false)
+    private Integer andar;
+
+    @Column(nullable = false)
+    private Integer capacidade;
 }
