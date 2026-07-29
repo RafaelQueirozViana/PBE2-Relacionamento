3f7cacd91015ea55a200eebd8438839203e9a255
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-07-29 15:05:37 -0300

    criando constructor e atributos da classe Hospede

--- src/main/java/com/recanto/model/Hospede.java
+++ src/main/java/com/recanto/model/Hospede.java
@@ -1,4 +1,39 @@
 package com.recanto.model;
 
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "hospede")
 public class Hospede {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(length = 20)
+    private String telefone;
+
+    @Column(length = 150)
+    private String email;
+
+    @OneToMany(mappedBy = "hospede", cascade = CascadeType.ALL)
+    private List<Reserva> reservas = new ArrayList<>();
+
+    public Hospede() {
+    }
+
+    public Hospede(String nome, String cpf, String telefone, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+    }
 }
