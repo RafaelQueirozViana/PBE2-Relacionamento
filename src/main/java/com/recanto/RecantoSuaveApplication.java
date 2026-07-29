d4e35afb72b2fbddb700bc6d594ac0510dad4072
Matheus Zacarias <matheuus412@gmail.com>
2026-07-29 11:27:19 -0300

    feat: initial configs

---
+++ src/main/java/com/recanto/RecantoSuaveApplication.java
@@ -0,0 +1,13 @@
+package com.recanto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecantoSuaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecantoSuaveApplication.class, args);
+	}
+
+}
